639d5561ef3872b2faf474d55de794354ae7f461
Uznain Rashid <147235060+uznainrashid@users.noreply.github.com>
2026-04-11 18:08:46 +0500

    changing done

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 5000;
 connectDB()
 // Cors settings
 app.use(cors({
-    origin:"http://localhost:3000",
+    origin:"https://chat-app-eta-one-62.vercel.app",
 }))
 app.use(express.json())
 // Get request
@@ -29,7 +29,7 @@ app.use("/api/auth", authRoutes)
 app.use("/api/messages", MessageRouter)
 const io = new Server(httpServer,{ 
     cors:{ 
-        origin:"http://localhost:3000", 
+        origin:"https://chat-app-eta-one-62.vercel.app", 
         methods:["GET","POST"],
         allowedHeaders: ["authorization"],
        credentials: true
